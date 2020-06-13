eafc431f7c4f97dcf2b83b6115cccfe82e06c423
alanmtz10 <bronkbsgck@gmail.com>
2020-06-13 02:25:44 -0500

    Add styles and scripts

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,23 +8,38 @@ namespace VentaPlantasEcommerce
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                       "~/Scripts/jquery.min.js",
+                       "~/Scripts/jquery-migrate-3.0.1.min.js",
+                       "~/Scripts/popper.min.js",
+                       "~/Scripts/bootstrap.min.js",
+                       "~/Scripts/jquery.easing.1.3.js",
+                       "~/Scripts/jquery.waypoints.min.js",
+                       "~/Scripts/jquery.stellar.min.js",
+                       "~/Scripts/owl.carousel.min.js",
+                       "~/Scripts/jquery.magnific-popup.min.js",
+                       "~/Scripts/aos.js",
+                       "~/Scripts/jquery.animateNumber.min.js",
+                       "~/Scripts/bootstrap-datepicker.js",
+                        "~/Scripts/scrollax.min.js",
+                        "~/Scripts/main.js"
+                       ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/open-iconic-bootstrap.min.css",
+                      "~/Content/css/animate.css",
+                      "~/Content/css/owl.carousel.min.css",
+                      "~/Content/css/owl.theme.default.min.css",
+                      "~/Content/css/magnific-popup.css",
+                      "~/Content/css/aos.css",
+                      "~/Content/css/ionicons.min.css",
+                      "~/Content/css/bootstrap-datepicker.css",
+                      "~/Content/css/jquery.timepicker.css",
+                      "~/Content/css/flaticon.css",
+                      "~/Content/css/icomoon.css",
+                      "~/Content/css/style.css"
+                      ));
         }
     }
 }
